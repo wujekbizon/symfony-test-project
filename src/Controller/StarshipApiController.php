0f05fd2144e2477c9e66a3ea05a047abc74ca644
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2024-08-19 21:40:25 +0200

    Initialize symfony project

---
+++ src/Controller/StarshipApiController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\StarshipRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class StarshipApiController extends AbstractController
+{
+  #[Route('/api/starships')]
+  public function getCollection(StarshipRepository $repository): Response
+  {
+    $starships = $repository->findAll();
+
+    return $this->json($starships);
+  }
+}
---
+++ src/Model/Starship.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Model;
+
+final readonly class Starship
+{
+  public function __construct(
+    public int    $id,
+    public string $name,
+    public string $class,
+    public string $captain,
+    public string $status,
+  ) {}
+}
