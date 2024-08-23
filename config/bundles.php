3759ab1265297e3ed190f9e83e160affd34009d3
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2024-08-23 08:45:42 +0200

    Add stymulus and turbo package, replace static status data  with dynamic

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,7 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
--- src/Model/Starship.php
+++ src/Model/Starship.php
@@ -11,4 +11,18 @@ final readonly class Starship
     public string $captain,
     public StarshipStatusEnum $status,
   ) {}
+
+  public function getStatusString(): string
+  {
+    return $this->status->value;
+  }
+
+  public function getStatusImageFilename(): string
+  {
+    return match ($this->status) {
+      StarshipStatusEnum::WAITING => 'images/status-waiting.png',
+      StarshipStatusEnum::IN_PROGRESS => 'images/status-in-progress.png',
+      StarshipStatusEnum::COMPLETED => 'images/status-complete.png',
+    };
+  }
 }
